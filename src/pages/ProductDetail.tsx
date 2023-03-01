20852e2960fdde80eb08565a951e63686833041d
James Bailey <jbailey@codisms.com>
2023-03-01 21:15:57 +0000

    Trading tooltip

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import {
+  IonModal,
   IonContent,
   IonHeader,
   IonPage,
@@ -31,6 +32,8 @@ const ProductDetail: React.FC = () => {
   const [imageIndex, setImageIndex] = useState<number>(0);
   const [showPrevious, setShowPrevious] = useState<boolean>(false);
   const [showNext, setShowNext] = useState<boolean>(false);
+
+  const modal = useRef<HTMLIonModalElement>(null);
 
   console.log('ProductDetail:', productId);
 
@@ -116,10 +119,6 @@ const ProductDetail: React.FC = () => {
 
   function message() {
     console.log('message');
-  }
-
-  function showTradeTooltip() {
-    console.log('show trade tooltip');
   }
 
   return (
@@ -270,7 +269,7 @@ const ProductDetail: React.FC = () => {
           <IonGrid className="trade-tooltip" onClick={(e) => {
             e.preventDefault();
             e.stopPropagation();
-            showTradeTooltip();
+            modal.current?.present();
           }}>
             <IonRow>
               <IonCol>
@@ -473,7 +472,84 @@ const ProductDetail: React.FC = () => {
           </IonList>
           */}
         </IonContent>
+
       ) : ''}
+
+      <IonModal className="trade-tooltip-container" ref={modal} backdropDismiss={false} swipeToClose={false}>
+        <IonContent>
+          <IonGrid className="trade-tooltip">
+            <IonRow className="header">
+              <IonCol>
+                TRADING ON <i>The</i>NOBO
+              </IonCol>
+            </IonRow>
+            <IonRow className="bullet">
+              <IonCol size="1" className="label">
+                1.
+              </IonCol>
+              <IonCol size="11" className="value">
+                <h1>List Your Product for Trade</h1>
+                <p>Trade products from your closet, quickly and easily!</p>
+              </IonCol>
+            </IonRow>
+            <IonRow className="bullet">
+              <IonCol size="1" className="label">
+                2.
+              </IonCol>
+              <IonCol size="11" className="value">
+                <h1>Get Paid</h1>
+                <p>When the other NOBO user accepts your trade request and both of you split the cost of the trade transaction fee, we will ship your product to them, and theirs to you! Hang tight, your “new for you” luxury product is on the way!</p>
+              </IonCol>
+            </IonRow>
+            <IonRow className="table-container">
+              <IonCol>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Trade Transaction Value</th>
+                      <th>Trade Transaction Fee</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>$1 - $19,999</td>
+                      <td>12%</td>
+                    </tr>
+                    <tr>
+                      <td>$20,000 - $49,999</td>
+                      <td>8%</td>
+                    </tr>
+                    <tr>
+                      <td>$50,000 +</td>
+                      <td>4%</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </IonCol>
+            </IonRow>
+            <IonRow className="bullet">
+              <IonCol size="1" className="label">
+                3.
+              </IonCol>
+              <IonCol size="11" className="value">
+                <h1>Support</h1>
+                <p>We’re here to help, in any way we can.</p>
+                <p>Contact us: <a href="mailto:customerservice@thenobo.com">customerservice@thenobo.com</a></p>
+                <p>we promise you’ll hear from real people, not a bot!</p>
+              </IonCol>
+            </IonRow>
+            <IonRow className="close" onClick={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              modal.current?.dismiss();
+            }}>
+              <IonCol>
+                Close
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </IonContent>
+      </IonModal>
     </IonPage>
   );
 };
