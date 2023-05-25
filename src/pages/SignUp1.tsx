45f812b2d83971b690563606a5bec48d6da61438
obaro Paul <105018533+bettercallpaul22@users.noreply.github.com>
2023-05-25 12:58:42 +0100

    signup2 header

--- src/pages/SignUp1.tsx
+++ src/pages/SignUp1.tsx
@@ -81,116 +81,98 @@ const SignUp1: React.FC = () => {
 					{/* </IonToolbar> */}
 				</IonHeader>
 
-				<div className='signup-details-wrapper'>
-					<IonRow style={{ marginTop: '7.9vh' }}>
-						<IonCol className='signup-details-nobo-logo' size='12'>
-							<img height={65} src="assets/images/nobo-logo-white.png" alt="logo" />
+				<IonRow style={{ marginTop: '6vh' }}>
+					<IonCol className='signup-details-nobo-logo' size='12'>
+						<img height={65} src="assets/images/nobo-logo-white.png" alt="logo" />
+					</IonCol>
+				</IonRow>
+
+				<IonGrid className="signup-details-box">
+					<IonRow   >
+						<IonCol className='signup-details-title' size='12'	>
+							SIGN UP
 						</IonCol>
 					</IonRow>
+					<IonRow className='signup-details-input-container'>
+						<IonCol>
+							<Input
+								type="text"
+								value={firstName}
+								className={`nobo-input`}
+								placeholder="FIRST NAME"
+								onChange={val => {
+									setFirstName(val);
+								}}
+							/>
+						</IonCol>
+					</IonRow>
+					<IonRow className='signup-details-input-container'>
+						<IonCol>
+							<Input
+								invalid={error}
+								value={lastName}
+								className={`nobo-input `}
+								placeholder="LASTNAME"
+								type="text"
+								onChange={val => {
+									setLastName(val);
+								}}
+							></Input>
+						</IonCol>
+					</IonRow>
+					<IonRow className='signup-details-input-container'>
+						<IonCol>
+							<Input
+								invalid={error}
+								value={email}
+								className={`nobo-input ${error ? 'invalid-text-color' : ''}`}
+								placeholder="EMAIL ADDRESS"
+								type="email"
+								errorMessage={error ? 'Email already in use' : ''}
+								onChange={val => {
+									setEmail(val);
+									if (!emailCheck(val)) {
+										setEmailError(true);
+									} else {
+										setEmailError(false);
+									}
+								}}
+							></Input>
+						</IonCol>
+					</IonRow>
+					<IonRow >
+						<IonCol className='signup-details-terms'>
+							By selecting agree and continue below, I agree to the
+							<IonCol style={{ color: 'goldenrod' }}> Terms of Service and Privacy</IonCol>
+						</IonCol>
+					</IonRow>
+					<IonRow className='signup-details-btn'>
+						<IonCol>
+							<Button
+								onClick={() => {
+									checkUserExist();
+								}}
+								label="AGREE AND CONTINUE"
+								large={true}
+								className=""
+								disabled={!validate()}
+							/>
+						</IonCol>
+					</IonRow>
+					<IonRow >
+						<IonCol className='already-have-account' size="8">
+							Already have an account?
+						</IonCol>
+						<IonCol
 
-					<IonGrid
-						className="signup-details-box"
-					>
-						<IonRow   >
-							<IonCol className='signup-details-title' size='12'	>
-								SIGN UP
-							</IonCol>
-						</IonRow>
-						<IonRow>
-							<IonCol>
-								<Input
-									type="text"
-									value={firstName}
-									className={`nobo-input`}
-									placeholder="FIRST NAME"
-									onChange={val => {
-										setFirstName(val);
-									}}
-								/>
-							</IonCol>
-						</IonRow>
-						<IonRow>
-							<IonCol>
-								<Input
-									invalid={error}
-									value={lastName}
-									className={`nobo-input `}
-									placeholder="LASTNAME"
-									type="text"
-									onChange={val => {
-										setLastName(val);
-									}}
-								></Input>
-							</IonCol>
-						</IonRow>
-						<IonRow>
-							<IonCol>
-								<Input
-									invalid={error}
-									value={email}
-									className={`nobo-input ${error ? 'invalid-text-color' : ''}`}
-									placeholder="EMAIL ADDRESS"
-									type="email"
-									errorMessage={error ? 'Email already in use' : ''}
-									onChange={val => {
-										setEmail(val);
-										if (!emailCheck(val)) {
-											setEmailError(true);
-										} else {
-											setEmailError(false);
-										}
-									}}
-								></Input>
-							</IonCol>
-						</IonRow>
-						<IonRow>
-							<IonCol>
-								By selecting agree and continue below, I agree to the
-								<IonCol style={{ color: 'goldenrod' }}> Terms of Service and Privacy</IonCol>
-							</IonCol>
-						</IonRow>
-						<IonRow>
-							<IonCol>
-								<Button
-									onClick={() => {
-										checkUserExist();
-									}}
-									label="REGISTER"
-									large={true}
-									className=""
-									disabled={!validate()}
-								/>
-							</IonCol>
-						</IonRow>
-						<IonRow style={{ paddingBottom: 12 }}>
-							<IonCol
-								size="8"
-								style={{
-									fontFamily: 'Nunito Sans',
-									fontWeight: '600',
-									fontSize: '15px',
-									lineHeight: '22.5px',
-									color: '#FFFFFF',
-								}}
-							>
-								already have an account?
-							</IonCol>
-							<IonCol
-								onClick={() => history.push('/login')}
-								size="4"
-								style={{
-									textDecorationLine: 'underline',
-									fontFamily: 'Nunito Sans',
-									fontWeight: '700',
-									color: '#D6980E',
-									fontSize: '15px',
-								}}
-							>
-								Sign In
-							</IonCol>
-						</IonRow>
-					</IonGrid>
-				</div>
+						className='signup-details-signin'
+						onClick={() => history.push('/login')} size="4"
+						>
+							<p style={{ marginLeft:'40px'  }}> SIGN IN </p>
+						</IonCol>
+					</IonRow>
+				</IonGrid>
+
 			</div>
 
 		</IonPage>
