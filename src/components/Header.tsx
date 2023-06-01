e92b74c836d780ddbbfe5d6391538a1e29ad640d
James Bailey <jbailey@codisms.com>
2023-06-01 15:06:02 -0700

    Fix header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,27 +20,30 @@ const Header: React.FC<HeaderProps> = ({
 
   return (
     <IonHeader className={'page-header ' + (border ? '' : 'no-border')}>
-      {showBackButton && (
-        <img
-          src="assets/images/arrow-left.svg"
-          className="back-arrow"
-          alt="back"
-          onClick={() => {
-            history.goBack();
-          }}
-        />
-      )}
-      <div className="titles">
-        {subtitle ? (
-          <>
-            <div className="subtitle">{subtitle}</div>
-            <div className="title">{title}</div>
-          </>
-        ) : (
-          <div className="title">{title}</div>
+      <div className="page-header-padding"></div>
+      <div className="page-header-container">
+        {showBackButton && (
+          <img
+            src="assets/images/arrow-left.svg"
+            className="back-arrow"
+            alt="back"
+            onClick={() => {
+              history.goBack();
+            }}
+          />
         )}
+        <div className="titles">
+          {subtitle ? (
+            <>
+              <div className="subtitle">{subtitle}</div>
+              <div className="title">{title}</div>
+            </>
+          ) : (
+            <div className="title">{title}</div>
+          )}
+        </div>
+        {children}
       </div>
-      {children}
     </IonHeader>
   );
 };
