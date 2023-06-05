178475c8453186ab7a91bc00e088d5213af82687
obaro Paul <105018533+bettercallpaul22@users.noreply.github.com>
2023-06-05 09:28:59 +0100

    added keyboard config in congig file

--- src/pages/TradeCompleted.tsx
+++ src/pages/TradeCompleted.tsx
@@ -38,17 +38,17 @@ const TradeCompleted: React.FC = () => {
       .catch((err) => { console.log('err info while fetching products ', err) })
   })
 
-  const acceptTrade = (productId: string) => {
-    console.log(productId)
-    userService.acceptTradeOffer(productId)
-      .then((res) => {
-        console.log(res)
-        history.push({ pathname: `trades/accepted/${productId}`, state: res })
-      })
-      .catch((error) => {
-        console.log('error accepting trade : ', error)
-      })
-  }
+  // const acceptTrade = (productId: string) => {
+  //   console.log(productId)
+  //   userService.acceptTradeOffer(productId)
+  //     .then((res) => {
+  //       console.log(res)
+  //       history.push({ pathname: `trades/accepted/${productId}`, state: res })
+  //     })
+  //     .catch((error) => {
+  //       console.log('error accepting trade : ', error)
+  //     })
+  // }
 
 
 
@@ -56,7 +56,7 @@ const TradeCompleted: React.FC = () => {
 
 
 
-  console.log(tradesData[0]?.received)
+  console.log(tradesData)
   return (
     <IonPage className='trade-completed-main-container'>
       <IonRow>
@@ -109,8 +109,8 @@ const TradeCompleted: React.FC = () => {
                     {<img
                       className='trade-completed-img-l'
                       src={product.products.requested.images[0]?.url.length < 60 ?
-                         `https://staging.thenobo.com/${product.products.requested.images[0].url}` :
-                          `${product.products.requested.images[0].url}`} alt=''
+                        `https://staging.thenobo.com/${product.products.requested.images[0].url}` :
+                        `${product.products.requested.images[0].url}`} alt=''
                     />}
                     {<img
                       className='trade-completed-img-r'
